4961378fd3787578d15a74dcd3f13e398607f9e7
gar354 <115901590+gar354@users.noreply.github.com>
2023-02-26 09:21:29 -0800

    making shid cleaner

--- scripts/UI.gd
+++ scripts/UI.gd
@@ -1,10 +1,15 @@
-extends Node2D
+extends Control
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
+
+export var file_dialog_path : NodePath
+onready var file_dialog : FileDialog = get_node(file_dialog_path)
+export var confirmation_dialog_path : NodePath
+onready var confirmation_dialog: ConfirmationDialog = get_node(confirmation_dialog_path)
 signal play_auton
 signal go_to(side)
 
@@ -13,26 +18,30 @@ signal focus_return
 
 var file_path: String
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
+	file_dialog.connect("file_selected",self,"file_dialog_file_selected")
+	file_dialog.connect("popup_hide",self,"file_dialog_popup_hide")
+	confirmation_dialog.connect("confirmed",self,"confirmation_dialog_confirmed")
+	
 	AutonPath.connect("path_changed", self, "_on_path_changed")
 	for node in get_tree().get_nodes_in_group("numbers"):
 		node.connect("text_changed", self, "_on_text_changed", [node.name])
 
 	for node in get_tree().get_nodes_in_group("buttons"):
 		node.connect("pressed", self, "_on_button_press", [node])
-	pass # Replace with function body.
 
-func _on_path_changed():
+func _on_path_changed() -> void:
 	var i = AutonPath.focused_node
 	get_tree().get_nodes_in_group("title")[0].text = "pont " + str(i)
 	for node in get_tree().get_nodes_in_group("numbers"):
 		var val = AutonPath.current_path[i]
-		var p = node.caret_position
+#		var p = node.line_edit.caret_position
 		for s in node.name.split("->"):
 			val = val[s]
-		node.text = str(val)
-		node.caret_position = p
+		if val == null:
+			return
+		node.value = val
+#		node.caret_position = p
 
 func _on_text_changed(new_text, node_name):
 	if new_text == "":
@@ -40,9 +49,9 @@ func _on_text_changed(new_text, node_name):
 	var i = AutonPath.focused_node
 	if i != 0:
 		if node_name == "pos->x":
-			return AutonPath.set_point(i, Vector2(float(new_text), AutonPath.current_path[i].pos.y))
-		if node_name == "pos->y":
-			return AutonPath.set_point(i, Vector2(AutonPath.current_path[i].pos.x, float(new_text)))
+			AutonPath.set_point(i, Vector2(float(new_text), AutonPath.current_path[i].pos.y))
+		elif node_name == "pos->y":
+			AutonPath.set_point(i, Vector2(AutonPath.current_path[i].pos.x, float(new_text)))
 	
 	if new_text.to_upper() == "NULL":
 		AutonPath.current_path[i][node_name] = null
@@ -50,7 +59,7 @@ func _on_text_changed(new_text, node_name):
 	
 	AutonPath.current_path[i][node_name] = new_text as int
 
-func _on_button_press(node: Button):
+func _on_button_press(node: Button) -> void:
 	var i = AutonPath.focused_node
 	match node.text:
 		"LEFT":
@@ -60,34 +69,30 @@ func _on_button_press(node: Button):
 		"delete node":
 			AutonPath.remove_point(i)
 		"clear":
-			$CanvasLayer/Control/ConfirmationDialog.popup()
+			confirmation_dialog.popup()
 			emit_signal("focus_leave")
 		"play":
 			emit_signal("play_auton")
 		"save":
 			emit_signal("focus_leave")
-			$CanvasLayer/Control/FileDialog.mode = 4
-			$CanvasLayer/Control/FileDialog.popup()
+			file_dialog.mode = FileDialog.MODE_SAVE_FILE
+			file_dialog.popup()
 		"load":
 			emit_signal("focus_leave")
-			$CanvasLayer/Control/FileDialog.mode = 0
-			$CanvasLayer/Control/FileDialog.popup()
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+			file_dialog.mode = FileDialog.MODE_OPEN_FILE
+			file_dialog.popup()
 
-
-func _on_ConfirmationDialog_confirmed():
+func confirmation_dialog_confirmed() -> void:
 	AutonPath.clear_path()
 	emit_signal("focus_return")
 
-func _on_FileDialog_popup_hide():
+func file_dialog_popup_hide() -> void:
 	emit_signal("focus_return")
 
-func _on_FileDialog_file_selected(path):
-	if $CanvasLayer/Control/FileDialog.mode == 4:
+func file_dialog_file_selected(path : String) -> void:
+	if file_dialog.mode == 4:
 		AutonPath.save_to(path)
 	else:
 		AutonPath.load_from(path)
 	emit_signal("focus_return")
-	$CanvasLayer/Control/FileDialog.hide()
+	file_dialog.hide()
